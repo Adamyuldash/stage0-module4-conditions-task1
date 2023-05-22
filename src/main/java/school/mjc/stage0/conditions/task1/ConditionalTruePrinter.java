5e031458dc4944e110ff8fd5f0a3de6b05c48344
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-22 11:40:23 -0700

    ahaha

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean isTrue = true;
+
+        if (isTrue) {
+            System.out.println("true");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/NoBracketsPrinter.java
@@ -1,9 +1,13 @@
 package school.mjc.stage0.conditions.task1;
-
 public class NoBracketsPrinter {
     public static void main(String[] args) {
-        if(false)
+        if (true) {
+        } else {
+
+
             System.out.println("The statement is true");
-        System.out.println("I'm out of checking");
+            System.out.println("I'm out of checking");
+        }
     }
+
 }
